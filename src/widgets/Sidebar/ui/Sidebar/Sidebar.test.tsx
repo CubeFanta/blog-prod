bd85492409070adf6d76093ffe0328f6011a7456
CubeFanta <merch.vito@gmail.com>
2023-03-09 00:50:05 +0300

    add fix import ts, tsx

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
-import { Sidebar } from './Sidebar';
 import { withTranslation } from 'react-i18next';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { Sidebar } from './Sidebar';
 // import { Sidebar } from '';
 
 describe('Sidebar', () => {
 	test('Test clear theme', () => {
 		// eslint-disable-next-line i18next/no-literal-string
-		renderWithTranslation(<Sidebar />)
+		renderWithTranslation(<Sidebar />);
 		expect(screen.getByTestId('sidebar')).toBeInTheDocument();
 	});
 });
