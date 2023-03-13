76953102cc4f9b5c479098692731a14407b2c470
CubeFanta <merch.vito@gmail.com>
2023-03-13 15:40:51 +0300

    add checks

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,28 +1,30 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
 	className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
+const { t } = useTranslation();
 
-	<div className={classNames(cls.navbar, {}, [className])}>
+export const Navbar = ({ className }: NavbarProps) => (
+	< div className={classNames(cls.navbar, {}, [className])}>
 		<div className={cls.links}>
 			<AppLink
 				theme={AppLinkTheme.SECONDARY}
 				to="/"
 				className={cls.mainLink}
 			>
-				Главная
+				{t('Главная')}
 			</AppLink>
 			<AppLink
 				theme={AppLinkTheme.SECONDARY}
 				to="/about"
 			>
-				О сайте
+				{t('О сайте')}
 			</AppLink>
 		</div>
-	</div>
+	</ >
 );
