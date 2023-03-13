fb550f868d7d3a85f5643852163d5e9366cb6044
CubeFanta <merch.vito@gmail.com>
2023-03-13 18:15:40 +0300

    add fix error

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,30 +1,33 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import cls from './Navbar.module.scss';
 import { useTranslation } from 'react-i18next';
+import cls from './Navbar.module.scss';
+
 
 interface NavbarProps {
 	className?: string;
 }
 
-const { t } = useTranslation();
+export const Navbar = ({ className }: NavbarProps) => {
+	const { t } = useTranslation();
 
-export const Navbar = ({ className }: NavbarProps) => (
-	< div className={classNames(cls.navbar, {}, [className])}>
-		<div className={cls.links}>
-			<AppLink
-				theme={AppLinkTheme.SECONDARY}
-				to="/"
-				className={cls.mainLink}
-			>
-				{t('Главная')}
-			</AppLink>
-			<AppLink
-				theme={AppLinkTheme.SECONDARY}
-				to="/about"
-			>
-				{t('О сайте')}
-			</AppLink>
+	return (
+		<div className={classNames(cls.navbar, {}, [className])}>
+			<div className={cls.links}>
+				<AppLink
+					theme={AppLinkTheme.SECONDARY}
+					to="/"
+					className={cls.mainLink}
+				>
+					{t('Главная')}
+				</AppLink>
+				<AppLink
+					theme={AppLinkTheme.SECONDARY}
+					to="/about"
+				>
+					{t('О сайте')}
+				</AppLink>
+			</div>
 		</div>
-	</ >
-);
+	);
+}
