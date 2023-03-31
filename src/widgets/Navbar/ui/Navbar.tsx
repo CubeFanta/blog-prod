3f274724e5b8527e3ab538507d23b9373c188952
CubeFanta <merch.vito@gmail.com>
2023-03-31 14:31:33 +0300

    add login form

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,9 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Modal } from 'shared/ui/Modal/Modal';
+// import { Modal } from 'shared/ui/Modal/Modal';
 import { useCallback, useState } from 'react';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { LoginModal } from 'features/AuthByUsername/index';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
 	const { t } = useTranslation();
 	const [isAuthModal, setIsAuthModal] = useState(false);
 
-	const onToggleModal = useCallback(() => {
-		setIsAuthModal((prev) => !prev);
+	const onCloseModal = useCallback(() => {
+		setIsAuthModal(false);
+	}, []);
+
+	const onShowModal = useCallback(() => {
+		setIsAuthModal(true)
 	}, []);
 
 	return (
@@ -22,13 +27,17 @@ export const Navbar = ({ className }: NavbarProps) => {
 			<Button
 				theme={ThemeButton.CLEAR_INVERTED}
 				className={cls.links}
-				onClick={onToggleModal}
+				onClick={onShowModal}
 			>
 				{t('Войти')}
 			</Button>
-			<Modal isOpen={isAuthModal} onClose={onToggleModal}>
+			{/* <Modal isOpen={isAuthModal} onClose={onToggleModal}>
 				{t('texttexttexttexttexttexttexttext')}
-			</Modal>
+			</Modal> */}
+			<LoginModal
+				isOpen={isAuthModal}
+				onClose={onCloseModal}
+			/>
 		</div>
 	);
 };
